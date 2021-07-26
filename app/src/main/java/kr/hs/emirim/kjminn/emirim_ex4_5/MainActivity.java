83029acf2e632719361cfeae9147bc18b2d71bdb
kjminn <kjminn2001@gmail.com>
2021-07-26 13:39:46 +0900

    Edit MainActivity.java(Remove Item) Commit

--- app/src/main/java/kr/hs/emirim/kjminn/emirim_ex4_5/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kjminn/emirim_ex4_5/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtItem = findViewById(R.id.edit_item);
         arrList = new ArrayList<String>();
         ListView list1 = findViewById(R.id.list1);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrList);
@@ -30,8 +32,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 arrList.add(edtItem.getText().toString());
                 adapter.notifyDataSetChanged();
+                edtItem.setText("");
             }
         });
-
+        list1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                arrList.remove(position);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
     }
 }
